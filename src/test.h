aac932ada3dc20f5fee7fc41f4ff64d36ea928c5
Rasmus Andersson <rasmus@notion.se>
2013-03-01 14:50:27 -0800

    Added assertion functions to test.h: assert_not_eq, assert_eq_cstr and assert_null

--- src/test.h
+++ src/test.h
@@ -26,7 +26,7 @@ inline void _assert_eq(
     B value2, const char* name2,
     const char* source_name, int source_line)
 {
-  if (value1 != value2) {
+  if (!(value1 == value2)) {
     std::cerr << "\nAssertion failure at "
               << source_name << ":" << source_line << "\n"
               << "  " << name1 << " == " << name2 << "\n"
@@ -36,14 +36,54 @@ inline void _assert_eq(
   }
 }
 
+template <typename A, typename B>
+inline void _assert_not_eq(
+    A value1, const char* name1,
+    B value2, const char* name2,
+    const char* source_name, int source_line)
+{
+  if (!(value1 != value2)) {
+    std::cerr << "\nAssertion failure at "
+              << source_name << ":" << source_line << "\n"
+              << "  " << name1 << " != " << name2 << "\n"
+              << "  " << value1 << " != " << value2 << "\n"
+              ;
+    abort();
+  }
+}
+
+inline void _assert_eq_cstr(
+    const char* cstr1, const char* name1,
+    const char* cstr2, const char* name2,
+    const char* source_name, int source_line)
+{
+  if (std::strcmp(cstr1, cstr2) != 0) {
+    std::cerr << "\nAssertion failure at "
+              << source_name << ":" << source_line << "\n"
+              << "  " << name1 << " == " << name2 << "\n"
+              << "  \"" << cstr1 << "\" == \"" << cstr2 << "\"\n"
+              ;
+    abort();
+  }
+}
+
 #define assert_eq(a, b) \
   ::lum::_assert_eq((a), #a, (b), #b, LUM_FILENAME, __LINE__)
+
+#define assert_not_eq(a, b) \
+  ::lum::_assert_not_eq((a), #a, (b), #b, LUM_FILENAME, __LINE__)
+
+#define assert_eq_cstr(cstr, b) \
+  ::lum::_assert_eq_cstr((cstr), #cstr, (b), #b, LUM_FILENAME, __LINE__)
 
 #define assert_true(a) \
   ::lum::_assert_eq((a), #a, true, "true", LUM_FILENAME, __LINE__)
 
 #define assert_false(a) \
   ::lum::_assert_eq((a), #a, false, "false", LUM_FILENAME, __LINE__)
+
+#define assert_null(a) \
+  ::lum::_assert_eq((a), #a, (void*)NULL, "NULL", LUM_FILENAME, __LINE__)
 
 } // namespace lum
 #endif // __cplusplus

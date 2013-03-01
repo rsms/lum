df30e2446396eccf8ed3ad6ffd76cbc35de0ca48
Rasmus Andersson <rasmus@notion.se>
2013-03-01 14:13:35 -0800

    test assertion helpers

--- src/test.h
+++ src/test.h
@@ -11,7 +11,42 @@
 #if LUM_TEST_SUIT_RUNNING
   #define print(...) ((void)0)
 #else
-  #define print(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+  #define print(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
 #endif
+
+// ------------------------------
+#ifdef __cplusplus
+#include <iostream>
+
+namespace lum {
+
+template <typename A, typename B>
+inline void _assert_eq(
+    A value1, const char* name1,
+    B value2, const char* name2,
+    const char* source_name, int source_line)
+{
+  if (value1 != value2) {
+    std::cerr << "\nAssertion failure at "
+              << source_name << ":" << source_line << "\n"
+              << "  " << name1 << " == " << name2 << "\n"
+              << "  " << value1 << " == " << value2 << "\n"
+              ;
+    abort();
+  }
+}
+
+#define assert_eq(a, b) \
+  ::lum::_assert_eq((a), #a, (b), #b, LUM_FILENAME, __LINE__)
+
+#define assert_true(a) \
+  ::lum::_assert_eq((a), #a, true, "true", LUM_FILENAME, __LINE__)
+
+#define assert_false(a) \
+  ::lum::_assert_eq((a), #a, false, "false", LUM_FILENAME, __LINE__)
+
+} // namespace lum
+#endif // __cplusplus
+
 
 #endif  // _LUM_TEST_H_
